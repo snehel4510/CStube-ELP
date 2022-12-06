ea7cbd97da4b9ed273e62f9c81f00948a1c129b5
Snehel <sahilrps31@gmail.com>
2022-12-06 13:01:26 +0530

    build for production

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+
 require('dotenv').config()
 
 const userRoutes = require('./routes/user_routes');
@@ -18,6 +20,14 @@ app.use((req,res,next) => {
 // registering the routes
 app.use('/cstube/dash',dashRoutes)
 app.use('/cstube/user',userRoutes)
+
+// servbing the build folder
+app.use(express.static(path.join(__dirname,"../frontend/build")));
+
+app.get("*" , (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"))
+})
+
 
 // mongodb://localhost:27017/CSTUBE
 const port = process.env.PORT;
